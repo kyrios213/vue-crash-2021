2dceb1472b0e5109c004f5c06b4fca90d2e248bf
Lance Pidor <kyrios213@gmail.com>
2021-10-20 19:57:59 +0800

    First Commit

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="Click" class="btn" :style="{ background: color }">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    Click() {
+      console.log("Clicked");
+    },
+  },
+};
+</script>
